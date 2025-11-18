a49e0ce03313363326a6031daa9e00b08bae6ae3
lingmeng <wxzjt0318@gmail.com>
2025-11-18 13:42:44 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -20,6 +20,21 @@ import { LinkPreset } from "./types/config";
 const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
+		// ... 其他配置保持不变 ...
+
+	favicon: [
+		{
+			src: "/icon.png",      // 注意开头有斜杠！
+			sizes: "32x32",
+		},
+		{
+			src: "/icon.png",
+			sizes: "192x192",
+		},
+	],
+
+	// ... 后面的配置保持不变
+};
 	title: "灵梦的小站",
 	subtitle: "One Weblog website",
 	siteURL: "http://lm520.cc/", // 请替换为你的站点URL，以斜杠结尾
