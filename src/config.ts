20a84404a3d9bfeceb35acc7ba27907dc133ea30
lingmeng <wxzjt0318@gmail.com>
2025-11-18 14:02:37 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -274,8 +274,8 @@ export const navBarConfig: NavBarConfig = {
 };
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
-	name: "Matsuzaka Yuki",
-	bio: "The world is big, you have to go and see",
+	name: "lingmeng",
+	bio: "I like the world, because i want live",
 	typewriter: {
 		enable: true, // 启用个人简介打字机效果
 		speed: 80, // 打字速度（毫秒）
