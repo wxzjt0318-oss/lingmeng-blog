ffa887d06dbcc02f4316d202f9f8634e7b116c5b
lingmeng <wxzjt0318@gmail.com>
2025-11-20 16:03:32 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -520,7 +520,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 
 export const sakuraConfig: SakuraConfig = {
 	enable: true, // 默认关闭樱花特效
-	sakuraNum: 21, // 樱花数量
+	sakuraNum: 18, // 樱花数量
 	limitTimes: -1, // 樱花越界限制次数，-1为无限循环
 	size: {
 		min: 0.5, // 樱花最小尺寸倍数
